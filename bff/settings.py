d4e660b7214498efd8580ef0014f7ea19963d6c8
Michele Mattioni <mattions@gmail.com>
2013-07-10 21:04:42 +0100

    moved the templates to proper location

--- bff/settings.py
+++ bff/settings.py
@@ -113,11 +113,10 @@ ROOT_URLCONF = 'bff.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'bff.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+TEMPLATE_DIRS = [
+    os.path.join(PACKAGE_ROOT, "templates"),
+]
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
