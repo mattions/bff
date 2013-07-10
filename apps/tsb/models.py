4119013f1936b1c922c3f39a592632cf778fe511
Joe Foster <hoaxkey@gmail.com>
2013-07-10 20:18:09 +0100

    models

--- apps/tsb/models.py
+++ apps/tsb/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Sample(models.Model):
+    code = models.CharField(max_length=8, unique=True)
+
+    def __unicode__(self):
+        return self.code
